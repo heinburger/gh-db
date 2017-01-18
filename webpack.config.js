21280c2a70573599bb6d3ba995cc26087fd7b2a1
Steven Hein <steven.c.hein@gmail.com>
2017-01-18 18:22:41 +0100

    package mods

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const nodeEnv = process.env.NODE_ENV || 'development'
+const isProd = nodeEnv === 'production'
+const sourcePath = path.join(__dirname, './src')
+const staticsPath = __dirname
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const DashboardPlugin = require('webpack-dashboard/plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = function(env) {
+module.exports = function(){
+  console.log(isProd)
   return {
-    entry: './src/index.js',
+    context: sourcePath,
+    entry: {
+      app: './index.js',
+      vendor: ['react']
+    },
     output: {
-      path: `./build`,
-      filename: 'bundle.js'
+      path: staticsPath,
+      filename: '[name].bundle.js'
     },
     module: {
       rules: [{
@@ -25,12 +38,40 @@ module.exports = function(env) {
         exclude: /node_modules/
       }]
     },
+    resolve: {
+      extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js'],
+      modules: [
+        path.resolve(__dirname, 'node_modules'),
+        sourcePath
+      ]
+    },
     plugins: [
-      new ExtractTextPlugin('bundle.css'),
-      new HtmlWebpackPlugin({
-        template: './src/template.html'
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
       }),
-      new DashboardPlugin()
+      new ExtractTextPlugin('bundle.css'),
+      new HtmlWebpackPlugin({template: './template.html'}),
+      new webpack.LoaderOptionsPlugin({minimize: true}),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+        },
+        sourceMap: true,
+        output: {
+          comments: false
+        },
+      })
     ]
   }
 }
