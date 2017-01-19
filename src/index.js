79fc557661b4ca82aa97f9ec2abca6974d4c4f16
Steven Hein <steven.c.hein@gmail.com>
2017-01-19 19:05:14 +0100

    wow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {Provider} from 'mobx-react'
 
 import Root from './components/root'
+import dataStore from './stores/data'
 require('./styles/root.scss')
 
-render(<Root />, document.getElementById('root'))
+render(
+  (
+    <Provider dataStore={dataStore}>
+      <Root />
+    </Provider>
+  ),
+  document.getElementById('root')
+)
