bb1c388bbb907e3c2c82718b8731dfee70566be0
Steven Hein <steven.c.hein@gmail.com>
2017-08-31 19:11:43 +0200

    Add gh-db calls

---
+++ src/store.js
@@ -0,0 +1,28 @@
+import {observable, action} from 'mobx'
+import {getData} from '../api'
+
+const dataStore = observable({
+  items: [],
+  error: null,
+  loading: false,
+  isOnline: navigator.onLine,
+  checkOnline: action(() => {
+    dataStore.isOnline = navigator.onLine
+  }),
+  getData: action(() => {
+    dataStore.loading = true
+    dataStore.error = null
+    getData().then((data) => dataStore.dataSuccess(data))
+             .catch((err) => dataStore.dataFailure(err))
+  }),
+  dataSuccess: action((data) => {
+    dataStore.loading = false
+    dataStore.data = data
+  }),
+  dataFailure: action((err) => {
+    dataStore.loading = false
+    dataStore.error = err
+  })
+})
+
+export default dataStore
